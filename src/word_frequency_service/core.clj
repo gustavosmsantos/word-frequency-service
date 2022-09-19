b309c201dade22919ff6031ff68d38e46926a9f8
Gustavo Santos <gustavo.santos@brasilparalelo.com.br>
2022-09-18 21:08:53 -0300

    Including aero for config management

--- src/word_frequency_service/core.clj
+++ src/word_frequency_service/core.clj
@@ -1,11 +1,19 @@
 (ns word-frequency-service.core
   (:require
+   [aero.core :refer [read-config]]
+   [clojure.java.io :as io]
+   [clojure.string :as s]
    [com.stuartsierra.component :as component]
    [word-frequency-service.components.server :as server])
   (:gen-class))
 
-(defn base-system []
-  (-> (component/system-map :server (server/new-server {}))))
+(defn base-system
+  ([] (base-system (or (-> (System/getenv "PROFILE")
+                           s/lower-case
+                           keyword) :dev)))
+  ([profile]
+   (let [config (read-config (io/resource "config.edn") {:profile profile})]
+     (component/system-map :server (server/new-server (:server config))))))
 
 (defonce system (base-system))
 
@@ -16,4 +24,5 @@
   ;; utilitary for development purposes (repl evaluation)
   (do
     (alter-var-root #'system component/stop)
+    (alter-var-root #'system (constantly (base-system :dev)))
     (alter-var-root #'system component/start)))
