e2d6b977ffa54f1ed4d9c89aac7554898e246d02
Gustavo Santos <gustavo.santos@brasilparalelo.com.br>
2022-09-18 22:41:56 -0300

    Including missing controller docstring

--- src/word_frequency_service/controller.clj
+++ src/word_frequency_service/controller.clj
@@ -3,7 +3,11 @@
    [word-frequency-service.logic :as logic]))
 
 (defn word-frequency-count
-  ""
+  "Returns the word frequencies from a text, sorted by a descendent count.
+
+  Receives an optional top parameter to return an arbitrary amount of words.
+  This number should stay between 2 and 15. It considers the lower bound if
+  the param is not provided"
   [text top]
   (let [top (-> top
                 (or 2)
